d794bd969c9d64475d1e7ad5d8bb015e4fe5f27a
pricheta <pricheta@inbox.ru>
2026-03-21 10:04:33 +0800

    de

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
+
+
+class SimpleApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PyQt6 Simple App")
+        self.setGeometry(100, 100, 300, 200)
+
+        self.label = QLabel("Hello, PyQt6!", self)
+        self.button = QPushButton("Click Me", self)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.on_button_click)
+
+        self.setLayout(layout)
+        self.setWindowOpacity(0.3)
+
+    def on_button_click(self):
+        self.label.setText("Button clicked!")
+        self.hide()
+
+
+app = QApplication(sys.argv)
+
+# Create and show the main window
+window = SimpleApp()
+window.show()
+
+# Run the app
+sys.exit(app.exec())
